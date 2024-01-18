12fdf10fe391e8c4d84eca00bcccac9543f27a31
BattleCh1cken <BattleCh1cken@larkov.de>
2024-01-18 09:16:57 -0500

    Cleaned up flake

--- flake.nix
+++ flake.nix
@@ -31,27 +31,10 @@
           #pkgs.nodePackages_latest.mermaid-cli
           pkgs.act
           pkgs.typos
+          pkgs.just
+          pkgs.prettier
         ];
       };
 
-      packages.default = with pkgs;
-        stdenv.mkDerivation {
-          name = "notebook";
-          postConfigurePhase = ''
-            # mkdir -p src/fonts
-            # ln -s ${font-awesome}/share/fonts/opentype/* src/fonts/
-            # ln -s ${roboto}/share/fonts/truetype/* src/fonts/
-            # ln -s ${source-sans-pro}/share/fonts/truetype/* src/fonts/
-            # ln -s ${jetbrains-mono}/share/fonts/truetype/* src/fonts/
-            # ln -s ${nerdfonts.override {fonts = ["JetBrainsMono"];}}/share/fonts/truetype/* src/fonts/
-          '';
-
-          buildCommand = ''
-            mkdir $out
-            # ${typst}/bin/typst --font-path "${self}/src/fonts" compile ${self}/main.typ $out/main.pdf
-            ${typst}/bin/typst --font-path "${nerdfonts}/share/fonts/truetype/" fonts
-            ${typst}/bin/typst --font-path "${nerdfonts}/share/fonts/truetype/"  compile ${self}/main.typ $out/main.pdf
-          '';
-        };
     });
 }
